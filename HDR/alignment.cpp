c9a3e88b9e52dfc36baa00fec18e120431e2545f
KuPao <B10430029@mail.ntust.edu.tw>
2020-04-08 14:59:04 +0800

    modify alignment

--- HDR/alignment.cpp
+++ HDR/alignment.cpp
@@ -2,143 +2,26 @@
 
 void MTBA(std::vector<Image>& images, int n)
 {
-	cv::Mat sample = images[0].image.clone();
-	Brightness_Preserving(sample);
-	std::vector<int> move_x(n, 0);
-	std::vector<int> move_y(n, 0);
-
-	/*calculate offset for remaining image*/
-	for (int i = 1; i < n; i++) {
-		cv::Mat bp_image = images[i].image.clone();
-		Brightness_Preserving(bp_image);
-		/*start pyramid*/
-		for (int j = 5; j >= 0; j--) {
-			cv::Mat origin_clone = sample.clone();
-			cv::Mat current_clone = bp_image.clone();
-
-			cv::Mat sampimg = sample.clone();
-			cv::Mat otherimg = bp_image.clone();
-
-			cv::Mat samp = origin_clone.clone();
-			cv::Mat other = bp_image.clone();
-			cv::resize(samp, samp, cv::Size((int)(sample.cols / pow(2, j + 1)), (int)(sample.rows / pow(2, j + 1))));
-			cv::resize(samp, samp, cv::Size((int)(sample.cols / pow(2, j)), (int)(sample.rows / pow(2, j))));
-			cv::resize(other, other, cv::Size((int)(sample.cols / pow(2, j + 1)), (int)(sample.rows / pow(2, j + 1))));
-			cv::resize(other, other, cv::Size((int)(sample.cols / pow(2, j)), (int)(sample.rows / pow(2, j))));
-
-			for (int a = 0; a < samp.rows; a++) {
-				for (int b = 0; b < samp.cols; b++) {
-					if (samp.at<uchar>(a, b) != sampimg.at<uchar>(a, b)) {
-						origin_clone.at<uchar>(a, b) = 0;
-					}
-					if (other.at<uchar>(a, b) != otherimg.at<uchar>(a, b)) {
-						current_clone.at<uchar>(a, b) = 0;
-					}
-					if (sampimg.at<uchar>(a, b) != otherimg.at<uchar>(a, b)) {
-						current_clone.at<uchar>(a, b) = 0;
-					}
-
-				}
-			}
-
-			double total[9] = { 0 };
-
-			for (int a = 0; a < origin_clone.rows; a++) {
-				for (int b = 0; b < origin_clone.cols; b++) {
-					int count = 0;
-					for (int y = -1; y <= 1; y++) {
-						for (int x = -1; x <= 1; x++) {
-							if (b + move_x[i] + x > 0 && b + move_x[i] + x < current_clone.cols && a + move_y[i] + y > 0 && a + move_y[i] + y < current_clone.rows) {
-								if ((int)origin_clone.at<uchar>(a, b) != (int)current_clone.at<uchar>(a + move_y[i] + y, b + move_x[i] + x))
-									total[count]++;
-							}
-							count++;
-						}
-					}
-				}
-			}
-
-			int b = 0;
-
-			for (int a = 1; a < 9; a++)
-				if (total[b] > total[a])
-					b = a;
-
-			/*stay middle*/
-			if (total[b] * 1.5 >= total[4])
-				b = 4;
-
-			switch (b) {
-			case 0:
-				move_y[i] = (move_y[i] - 1) * 2;
-				move_x[i] = (move_x[i] - 1) * 2;
-				break;
-			case 1:
-				move_y[i] = (move_y[i] - 1) * 2;
-				break;
-			case 2:
-				move_y[i] = (move_y[i] - 1) * 2;
-				move_x[i] = (move_x[i] + 1) * 2;
-				break;
-			case 3:
-				move_x[i] = (move_x[i] - 1) * 2;
-				break;
-			case 4:
-				break;
-			case 5:
-				move_x[i] = (move_x[i] + 1) * 2;
-				break;
-			case 6:
-				move_y[i] = (move_y[i] + 1) * 2;
-				move_x[i] = (move_x[i] - 1) * 2;
-				break;
-			case 7:
-				move_y[i] = (move_y[i] + 1) * 2;
-				break;
-			case 8:
-				move_y[i] = (move_y[i] + 1) * 2;
-				move_x[i] = (move_x[i] + 1) * 2;
-				break;
-			default:
-				break;
-			}
-		}
-		/*end of pyramid*/
-	}
-	/*end of calculation*/
-
-	int min_x = *std::min_element(move_x.begin(), move_x.end());
-	int max_x = *std::max_element(move_x.begin(), move_x.end());
-	int min_y = *std::min_element(move_y.begin(), move_y.end());
-	int max_y = *std::max_element(move_y.begin(), move_y.end());
-
-	int b, i, j, k;
-#pragma parallel for private(b, i, j, k)
-	for (b = 0; b < n; b++) {
-		cv::Mat&& dest = cv::Mat::zeros(sample.rows + abs(min_y) + abs(max_y), sample.cols + abs(min_x) + abs(max_x), CV_8UC3);
-		for (i = 0; i < images[b].image.rows; i++) {
-			for (j = 0; j < images[b].image.cols; j++) {
-				for (k = 0; k < 3; k++) {
-					dest.at<cv::Vec3b>(i + abs(min_y) + move_y[b], j + abs(min_x) + move_x[b])[k] = images[b].image.at<cv::Vec3b>(i, j)[k];
-				}
-			}
-		}
-		images[b].image = dest.clone();
+	int shift_amt[2];
+	for (int i = 0; i < n - 1; i++) {
+		GetExpShift(images[i].image, images[i + 1].image, 4, shift_amt);
+		TranslateImage(images[i + 1].image, images[i + 1].image, shift_amt[0], shift_amt[1]);
 	}
 }
 
-void Brightness_Preserving(cv::Mat& image)
+void Brightness_Preserving(cv::Mat& src, cv::Mat& bp_image, cv::Mat& mask_img, int mask)
 {
 	int color_count[256] = { 0 };
+	bp_image = src.clone();
 	cv::Mat gray;
-	if (image.type() == CV_8UC3)
-		cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+	if (bp_image.type() == CV_8UC3)
+		cv::cvtColor(bp_image, gray, cv::COLOR_BGR2GRAY);
 
 	else
-		gray = image.clone();
+		gray = bp_image.clone();
 
-	for (int i = 0; i < image.rows; i++) {
-		for (int j = 0; j < image.cols; j++) {
+	for (int i = 0; i < bp_image.rows; i++) {
+		for (int j = 0; j < bp_image.cols; j++) {
 			color_count[gray.at<uchar>(i, j)]++;
 		}
 	}
@@ -146,7 +29,7 @@ void Brightness_Preserving(cv::Mat& image)
 	int count = 0;
 	double threshold = 0;
 	// find median
-	for (int i = 0; i < image.rows; i++) {
+	for (int i = 0; i < bp_image.rows; i++) {
 		count += color_count[i];
 
 		if (count >= (gray.cols * gray.rows) / 2) {
@@ -156,15 +39,84 @@ void Brightness_Preserving(cv::Mat& image)
 	}
 
 	cv::Mat&& dest = cv::Mat::zeros(gray.rows, gray.cols, CV_8UC1);
+	mask_img = cv::Mat::zeros(gray.rows, gray.cols, CV_8UC1);
 
-	for (int i = 0; i < image.rows; i++)
+	for (int i = 0; i < bp_image.rows; i++)
 	{
-		for (int j = 0; j < image.cols; j++)
+		for (int j = 0; j < bp_image.cols; j++)
 		{
 			gray.at<uchar>(i, j) > threshold ? dest.at<uchar>(i, j) = 255 : dest.at<uchar>(i, j) = 0;
+			gray.at<uchar>(i, j) >= threshold + mask &&
+				gray.at<uchar>(i, j) <= threshold - mask ? mask_img.at<uchar>(i, j) = 255 : mask_img.at<uchar>(i, j) = 0;
 		}
 	}
 
-	image = dest.clone();
+	bp_image = dest.clone();
 	gray.release();
+}
+
+cv::Mat TranslateImage(cv::Mat& img, cv::Mat& dst, int offsetx, int offsety) {
+	cv::Mat trans_mat = (cv::Mat_<double>(2, 3) << 1, 0, offsetx, 0, 1, offsety);
+	cv::warpAffine(img, dst, trans_mat, img.size());
+	return dst;
+}
+
+/*Compute the sum of all white pixel in the bitmap.*/
+int BitmapTotal(cv::Mat& img) {
+	int sum = 0;
+	for (int i = 0; i < img.rows; i++) {
+		for (int j = 0; j < img.cols; j++) {
+			sum += img.at<uchar>(i, j);
+		}
+	}
+	return sum / 255;
+}
+
+void GetExpShift(cv::Mat& img1, cv::Mat& img2, int shift_bits, int shift_ret[2]) {
+	int min_err;
+	int cur_shift[2];
+	cv::Mat tb1, tb2;
+	cv::Mat eb1, eb2;
+	int i, j;
+	if (shift_bits > 0) {
+		cv::Mat sml_img1, sml_img2;
+		cv::resize(img1, sml_img1, cv::Size((int)img1.cols / 2, (int)img1.rows / 2));
+		cv::resize(img2, sml_img2, cv::Size((int)img2.cols / 2, (int)img2.rows / 2));
+		GetExpShift(sml_img1, sml_img2, shift_bits - 1, cur_shift);
+		cur_shift[0] *= 2;
+		cur_shift[1] *= 2;
+	}
+	else {
+		cur_shift[0] = 0;
+		cur_shift[1] = 0;
+	}
+
+	Brightness_Preserving(img1, tb1, eb1, 4);
+	Brightness_Preserving(img2, tb2, eb2, 4);
+	min_err = img1.rows * img1.cols;
+
+	for (i = -1; i <= 1; i++) {
+		for (j = -1; j <= 1; j++) {
+			int xs = cur_shift[0] + i;
+			int ys = cur_shift[1] + j;
+			cv::Mat shifted_tb2;
+			cv::Mat shifted_eb2;
+			cv::Mat diff_b;
+			int err;
+			//printf("xs = %d, ys = %d\n",xs,ys);
+			(void)TranslateImage(tb2, shifted_tb2, xs, ys);
+			(void)TranslateImage(eb2, shifted_eb2, xs, ys);
+			cv::bitwise_xor(tb1, shifted_tb2, diff_b);
+			cv::bitwise_and(diff_b, eb1, diff_b);
+			cv::bitwise_and(diff_b, shifted_eb2, diff_b);
+			err = BitmapTotal(diff_b);
+			if (i == 0 && j == 0)
+				err -= 1;
+			if (err < min_err) {
+				shift_ret[0] = xs;
+				shift_ret[1] = ys;
+				min_err = err;
+			}
+		}
+	}
 }
--- HDR/alignment.h
+++ HDR/alignment.h
@@ -7,6 +7,9 @@
 #include "ImageData.h"
 
 void MTBA(std::vector<Image>& images, int n);
-void Brightness_Preserving(cv::Mat& image);
+void Brightness_Preserving(cv::Mat& src, cv::Mat& bp_image, cv::Mat& mask_img, int mask);
+cv::Mat TranslateImage(cv::Mat& img, cv::Mat& dst, int offsetx, int offsety);
+int BitmapTotal(cv::Mat& img);
+void GetExpShift(cv::Mat& img1, cv::Mat& img2, int shift_bits, int shift_ret[2]);
 
 #endif
